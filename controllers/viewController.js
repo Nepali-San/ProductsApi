add442df1bbc8e2e29bc527eceecff417d88c5ef
San Neupane <123here.comes.san@gmail.com>
2020-02-05 11:38:19 +0545

    layout structure change & overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,23 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', {
-        title: "Overview"
-    })
-}
+const Product = require('../models/productsModel')
+const catchAsync = require("../utils/catchAsync")
 
-exports.getProduct = (req, res) => {
-    res.status(200).render('product', {
-        title: "Product"
+exports.getOverview = catchAsync(async (req, res) => {
+    const products = await Product.find();
+
+    res.status(200).render('overview', {
+        title: "Overview",
+        products
     })
-}
+})
+
+exports.getProduct = catchAsync(async (req, res) => {
+    const product = await Product.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        field: 'review rating user'
+    })
+    res.status(200).render('product', {
+        title: "Product",
+        product
+
+    })
+})
